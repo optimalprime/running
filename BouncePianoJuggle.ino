789aa3ae19212b979e9f6d94dd41bde2273ab30d
Andy <optimalprime@users.noreply.github.com>
2014-10-01 23:39:24 -0400

    First release

---
+++ BouncePianoJuggle.ino
@@ -0,0 +1,61 @@
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = { NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3,
+                   NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = { 4, 8, 8, 4, 4, 4, 4 };
+
+const int LEDPIN = 13;
+const int knockSensor = A0;
+const int threshold = 1;
+const int piezoPIN = 3;
+
+int sensorReading = 0;
+int knockCounter = 0;
+int thisNote = 0;
+
+void setup()
+{
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(piezoPIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("sensorReading = ");
+  Serial.println(sensorReading);
+  Serial.print("Knock #");
+  Serial.println(knockCounter);
+  Serial.print("thisNote =");
+  Serial.println(thisNote);
+}
+
+void loop()
+{
+  sensorReading = analogRead(knockSensor);
+  
+  if (sensorReading >= threshold) {
+    if (7 > thisNote) {
+      int noteDuration = 2000 / noteDurations[thisNote];
+      tone(3, melody[thisNote], noteDuration);
+      digitalWrite(LEDPIN, HIGH);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(3);
+      digitalWrite(LEDPIN, LOW);
+      delay(30);
+      thisNote++;
+      knockCounter++;
+    } else {
+      thisNote = 0;
+    }
+    Serial.print("sensorReading = ");
+    Serial.println(sensorReading);
+    Serial.print("Knock #");
+    Serial.println(knockCounter);
+    Serial.print("thisNote =");
+    Serial.println(thisNote);
+ }
+
+ delay(10);
+}
+
